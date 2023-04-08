4c8a3b7bb6d570c873e55a332da41d78a6271447
Maksim Kalinin <kalinin.m.a@bk.ru>
2023-04-08 21:02:07 +0300

    Remove gulp config

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const svgSprite = require('./gulp-tasks/svg-sprite');
-const watch = require('./gulp-tasks/watch')
-
-gulp.task('svgSprite', svgSprite);
-gulp.task('watch', watch);
