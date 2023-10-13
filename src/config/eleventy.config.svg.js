992ce6ea183245944432bed3f0ff4cde8be380e1
Maksim Kalinin <kalinin.m.a@bk.ru>
2023-10-13 03:28:07 +0300

    Add SVGO transform

--- src/config/eleventy.config.svg.js
+++ src/config/eleventy.config.svg.js
@@ -1,4 +1,12 @@
+const { optimize, loadConfig } = require('svgo');
 const pluginIcons = require('eleventy-plugin-icons');
+
+const optimizeWithSVGO = async (content, configPath) => {
+	const config = await loadConfig(configPath);
+	const { data } = await optimize(content, config);
+
+	return data;
+};
 
 /** @param {import("@11ty/eleventy").UserConfig} config */
 module.exports = (config) => {
@@ -7,6 +15,7 @@ module.exports = (config) => {
 		sources: [{ name: 'custom', path: './src/assets/svg/', default: true }],
 		icon: {
 			shortcode: 'icon',
+			transform: (svg) => optimizeWithSVGO(svg, './svgo.config.js'),
 		},
 		sprite: {
 			shortcode: 'svgSprite',
